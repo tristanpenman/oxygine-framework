72e4564a7cd926122877fb32f16cf1d0435f0d8b
Tristan Penman <tristan@tristanpenman.com>
2023-06-17 14:16:52 +1000

    Handle running outside of an application bundle on macOS

--- oxygine/src/oxygine/core/ios/ios.mm
+++ oxygine/src/oxygine/core/ios/ios.mm
@@ -20,9 +20,13 @@ namespace oxygine
 		std::string getSupportFolder()
 		{
             NSString* bundleID = [[NSBundle mainBundle] bundleIdentifier];
+            if (!bundleID) {
+                return "";
+            }
+
             NSFileManager*fm = [NSFileManager defaultManager];
             NSURL*    dirPath = nil;
-            
+
             // Find the application support directory in the home directory.
             NSArray* appSupportDir = [fm URLsForDirectory:NSApplicationSupportDirectory
                                                 inDomains:NSUserDomainMask];
@@ -31,7 +35,7 @@ namespace oxygine
                 // Append the bundle ID to the URL for the
                 // Application Support directory
                 dirPath = [[appSupportDir objectAtIndex:0] URLByAppendingPathComponent:bundleID];
-                
+
                 // If the directory does not exist, this method creates it.
                 // This method is only available in OS X v10.7 and iOS 5.0 or later.
                 NSError*    theError = nil;
@@ -39,60 +43,60 @@ namespace oxygine
                                    attributes:nil error:&theError])
                 {
                     // Handle the error.
-                    
-                    return nil;
+
+                    return "";
                 }
             }
-            
+
             return dirPath.fileSystemRepresentation;
         }
 	}
-    
+
     namespace operations
     {
         void blit(const ImageData &src, const ImageData &dest);
         void blitPremultiply(const ImageData &src, const ImageData &dest);
     }
-    
+
 #if TARGET_OS_IPHONE
     bool nsImageLoad(Image &mt, void * pData, int nDatalen, bool premultiplied, TextureFormat destFormat)
     {
         NSData *data = [NSData dataWithBytesNoCopy:pData length:nDatalen];
         UIImage *image = [UIImage imageWithData:data];
-        
+
         GLuint width = (GLuint)CGImageGetWidth(image.CGImage);
         GLuint height = (GLuint)CGImageGetHeight(image.CGImage);
-        
+
         int bits = (int)CGImageGetBitsPerPixel(image.CGImage);
         int pitch = (int)CGImageGetBytesPerRow(image.CGImage);
-        
+
         CGImageAlphaInfo alpha = CGImageGetAlphaInfo(image.CGImage);
         TextureFormat srcFormat = bits == 32 ? TF_R8G8B8A8 : TF_R8G8B8;
-        
+
         if (destFormat == TF_UNDEFINED)
             destFormat = srcFormat;
 
-        
+
         Image temp;
         temp.init(width, height, srcFormat);
-        
-        
+
+
         ImageData src = temp.lock();
-        
+
         CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
         CGContextRef imgcontext = CGBitmapContextCreate( src.data, width, height, 8, src.pitch, colorSpace, kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big );
         CGColorSpaceRelease(colorSpace);
         CGContextClearRect(imgcontext, CGRectMake( 0, 0, width, height ) );
         //CGContextTranslateCTM( imgcontext, 0, height - height );
         CGContextDrawImage(imgcontext, CGRectMake( 0, 0, width, height ), image.CGImage );
-        
+
         CGContextRelease(imgcontext);
-        
+
         temp.unlock();
 
         mt.init(width, height, destFormat);
         ImageData dest = mt.lock();
-        
+
         if (!premultiplied)
         {
             operations::blitPremultiply(src, dest);
@@ -101,10 +105,10 @@ namespace oxygine
         {
             operations::blit(src, dest);
         }
-        
+
         mt.unlock();
-        
-        
+
+
         return true;
     }
 #else
@@ -114,16 +118,16 @@ namespace oxygine
         NSBitmapImageRep *image = [NSBitmapImageRep imageRepWithData:data];
         int w = image.size.width;
         int h = image.size.height;
-        
+
         TextureFormat srcFormat = image.bitsPerPixel == 32 ? TF_R8G8B8A8 : TF_R8G8B8;
         ImageData src(w, h, (int) image.bytesPerRow, srcFormat, image.bitmapData);
-        
+
         if (destFormat == TF_UNDEFINED)
             destFormat = srcFormat;
-        
+
         mt.init(w, h, destFormat);
         ImageData dest = mt.lock();
-        
+
         if (!premultiplied)
         {
             operations::blitPremultiply(src, dest);
@@ -132,14 +136,14 @@ namespace oxygine
         {
             operations::blit(src, dest);
         }
-        
+
         mt.unlock();
-        
-        
+
+
         return true;
     }
 #endif
-    
+
     void iosGetMemoryUsage(size_t &a)
     {
         struct task_basic_info info;
@@ -154,7 +158,7 @@ namespace oxygine
             a = 0;
         }
     }
-    
+
     void iosNavigate(const char *url_)
     {
 #if TARGET_OS_IPHONE
@@ -162,8 +166,8 @@ namespace oxygine
         [[UIApplication sharedApplication] openURL:url];
 #endif
     }
-    
-    
+
+
     int64 iosGetFreeDiskspace()
     {
         int64 totalSpace = 0;
@@ -171,7 +175,7 @@ namespace oxygine
         NSError *error = nil;
         NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
         NSDictionary *dictionary = [[NSFileManager defaultManager] attributesOfFileSystemForPath:[paths lastObject] error: &error];
-        
+
         if (dictionary) {
             NSNumber *fileSystemSizeInBytes = [dictionary objectForKey: NSFileSystemSize];
             NSNumber *freeFileSystemSizeInBytes = [dictionary objectForKey:NSFileSystemFreeSize];
@@ -180,8 +184,8 @@ namespace oxygine
             NSLog(@"Memory Capacity of %llu MiB with %llu MiB Free memory available.", ((totalSpace/1024ll)/1024ll), ((totalFreeSpace/1024ll)/1024ll));
         } else {
             NSLog(@"Error Obtaining System Memory Info: Domain = %@, Code = %ld", [error domain], (long)[error code]);
-        }  
-        
+        }
+
         return totalFreeSpace;
     }
 }
